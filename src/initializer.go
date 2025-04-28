fd6090ed1f19bfa305a4d1e383c89a01a418b080
fateee <itsfateee@gmail.com>
2025-04-28 18:49:32 +0300

    temp save

--- src/initializer.go
+++ src/initializer.go
@@ -9,15 +9,30 @@ var ingredientsMap map[string]Ingredient
 var potionsMap map[[5]uint16]Potion
 var potionsMapByName map[string]Potion
 
+var PotionsByMags [5][]NameWithMags
+
+type NameWithMags struct {
+	name string
+	mags [5]uint16
+}
+
 var In *bufio.Reader
 var Out *bufio.Writer
 
 func Initialize() {
 	ingredientsMap = make(map[string]Ingredient, len(Ingredients))
 	potionsMap = make(map[[5]uint16]Potion, len(Potions))
+	for i := range 5 {
+		PotionsByMags[i] = make([]NameWithMags, 0, len(Potions))
+	}
 
 	for _, ing := range Ingredients {
 		ingredientsMap[ing.Name] = ing
+		for i, mag := range ing.Magimints {
+			if mag != 0 {
+				PotionsByMags[i] = append(PotionsByMags[i], NameWithMags{ing.Name, ing.Magimints})
+			}
+		}
 	}
 
 	for _, pot := range Potions {
--- src/searcher.go
+++ src/searcher.go
@@ -5,11 +5,12 @@ type SearchResult struct {
 	Ingredients     map[string]uint16
 	TotalMagimints  uint16
 	NumberIngreds   uint16
-	Traits          []TraitStruct
+	Traits          []TraitStruct // uploads after
 }
 
 func SearchPerfectCombosByParams(minMags, maxMags, minIngr, maxIngr uint16, desiredPotion string) *[]SearchResult {
 	stack := make([]searchUnit, 0, 1000)
+	searchResult := make([]SearchResult, 0, 1000)
 	if pt, ok := potionsMapByName[desiredPotion]; ok {
 		stack = append(stack, searchUnit{desiredPotion: pt.Magimints})
 	} else {
@@ -20,11 +21,57 @@ func SearchPerfectCombosByParams(minMags, maxMags, minIngr, maxIngr uint16, desi
 	for len(stack) != 0 {
 		currentUnit := stack[len(stack)-1]
 		stack = stack[:len(stack)-1]
-		if currentUnit.numIngreds >= maxIngr || currentUnit.numMagimints >= maxMags {
+		if currentUnit.numIngreds > maxIngr || currentUnit.numMagimints > maxMags {
 			continue
 		}
-
+		magIdx, ok := getNeededMag(&currentUnit.desiredPotion, &currentUnit.magimints)
+		if !ok {
+			continue
+		}
+		if magIdx == 5 && currentUnit.numIngreds >= minIngr && currentUnit.numMagimints >= minMags { // good potion
+			searchResult = append(searchResult, SearchResult{
+				ResultingPotion: potionsMap[currentUnit.desiredPotion],
+				Ingredients:     currentUnit.ingridients,
+				TotalMagimints:  currentUnit.numMagimints,
+				NumberIngreds:   currentUnit.numIngreds,
+			})
+		}
+		//todo
 	}
+	return &searchResult
+}
+
+func getNeededMag(desiredPotion *[5]uint16, magimints *[5]uint16) (uint16, bool) {
+	var maxMagIdx, maxMagValue uint16 = 100, 0
+	for i, mag := range magimints {
+		if mag > 0 && (*desiredPotion)[i] == 0 {
+			return 0, false
+		}
+		if mag > maxMagValue {
+			maxMagIdx = uint16(i)
+			maxMagValue = mag
+		}
+	}
+	if maxMagIdx == 100 {
+		for i, val := range desiredPotion {
+			if val != 0 {
+				return uint16(i), true
+			}
+		}
+	}
+	delim := (*desiredPotion)[maxMagIdx]
+	if (*magimints)[maxMagIdx]%delim != 0 {
+		return maxMagIdx, true
+	}
+	unit := (*magimints)[maxMagIdx] / delim
+	for i, mag := range *magimints {
+		if (*desiredPotion)[i]*unit > mag {
+			return uint16(i), true
+		} else if (*desiredPotion)[i]*unit < mag {
+			return maxMagIdx, true
+		}
+	}
+	return 5, true
 }
 
 type searchUnit struct {
