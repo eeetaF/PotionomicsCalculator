030479462174c7f2927874393ec5ba0fabd99245
fateee <itsfateee@gmail.com>
2025-05-06 01:35:16 +0300

    ingreds added

--- src/ingredients.go
+++ src/ingredients.go
@@ -21,12 +21,16 @@ var Ingredients = []Ingredient{
 	{[5]uint16{0, 0, 18, 0, 0}, Slime, "Sack of Hive Slime", 21, []TraitStruct{}},
 	{[5]uint16{30, 0, 0, 0, 0}, Slime, "Antlered Jelly", 28, []TraitStruct{
 		{Sensation, false}, {Visual, true}}},
+	{[5]uint16{0, 0, 30, 0, 0}, Slime, "Sack of Composite Slime", 36, []TraitStruct{}},
 	{[5]uint16{9, 9, 12, 12, 0}, Slime, "Bubble Ooze", 60, []TraitStruct{}},
 	{[5]uint16{0, 0, 0, 48, 0}, Slime, "Feathered Gelatin", 62, []TraitStruct{
 		{Sensation, true}, {Aroma, false}}},
 	{[5]uint16{32, 0, 0, 32, 0}, Slime, "Shelled Pudding", 90, []TraitStruct{}},
 	{[5]uint16{15, 15, 15, 15, 0}, Slime, "Copper Dollop", 95, []TraitStruct{}},
+	{[5]uint16{0, 0, 44, 0, 22}, Slime, "Bedazzled Custard", 95, []TraitStruct{
+		{Visual, true}}},
 	{[5]uint16{24, 24, 24, 24, 0}, Slime, "Silver Dollop", 138, []TraitStruct{}},
+	{[5]uint16{33, 33, 33, 33, 0}, Slime, "Gold Dollop", 176, []TraitStruct{}},
 
 	// Plant
 	{[5]uint16{0, 6, 0, 0, 0}, Plant, "Mandrake Root", 6, []TraitStruct{}},
@@ -37,11 +41,13 @@ var Ingredients = []Ingredient{
 	{[5]uint16{0, 30, 0, 0, 0}, Plant, "Mandragon Root", 34, []TraitStruct{}},
 	{[5]uint16{0, 48, 0, 0, 0}, Plant, "Acid Rutabaga", 54, []TraitStruct{
 		{Taste, false}, {Sound, true}}},
+	{[5]uint16{0, 44, 0, 44, 0}, Plant, "Widowmaker Pepper", 999999, []TraitStruct{}},
 	{[5]uint16{0, 32, 0, 32, 0}, Plant, "Daredevil Pepper", 90, []TraitStruct{}},
 	{[5]uint16{10, 0, 20, 0, 30}, Plant, "Mosquito Plant", 105, []TraitStruct{}},
 	{[5]uint16{20, 20, 15, 0, 15}, Plant, "Squid Vine", 135, []TraitStruct{}},
 	{[5]uint16{16, 0, 40, 0, 40}, Plant, "Acid Pitfall Plant", 145, []TraitStruct{}},
 	{[5]uint16{24, 24, 24, 0, 24}, Plant, "Harpy's Snare", 150, []TraitStruct{}},
+	{[5]uint16{22, 0, 55, 0, 55}, Plant, "Barracuda Plant", 172, []TraitStruct{}},
 
 	// Flower
 	{[5]uint16{4, 0, 0, 0, 0}, Flower, "Fairy Flower Bulb", 14, []TraitStruct{
@@ -54,6 +60,8 @@ var Ingredients = []Ingredient{
 		{Aroma, true}}},
 	{[5]uint16{16, 0, 0, 0, 0}, Flower, "Bramble-Rose", 45, []TraitStruct{
 		{Sensation, true}, {Sound, true}}},
+	{[5]uint16{22, 0, 0, 0, 0}, Flower, "Inverted Bramble-Rose", 46, []TraitStruct{
+		{Sensation, true}, {Sound, true}}},
 	{[5]uint16{40, 0, 0, 20, 0}, Flower, "Fire Flower", 55, []TraitStruct{
 		{Aroma, false}}},
 	{[5]uint16{24, 0, 0, 8, 0}, Flower, "Djinn Blossom", 68, []TraitStruct{
@@ -61,6 +69,8 @@ var Ingredients = []Ingredient{
 	{[5]uint16{8, 24, 24, 0, 0}, Flower, "Courtier's Orchid", 72, []TraitStruct{
 		{Aroma, true}}},
 	{[5]uint16{0, 16, 0, 48, 0}, Flower, "Watchdog Daisy", 83, []TraitStruct{}},
+	{[5]uint16{11, 33, 0, 33, 0}, Flower, "Orchid of the Ice Princess", 116, []TraitStruct{
+		{Aroma, true}}},
 
 	// Fruit
 	{[5]uint16{6, 0, 0, 0, 0}, Fruit, "Feyberry", 4, []TraitStruct{}},
@@ -73,7 +83,14 @@ var Ingredients = []Ingredient{
 		{Taste, true}, {Sound, false}}},
 	{[5]uint16{0, 0, 0, 64, 0}, Fruit, "Rottermelon", 68, []TraitStruct{
 		{Visual, false}}},
+	{[5]uint16{0, 33, 11, 0, 0}, Fruit, "Nightmare Pomme", 72, []TraitStruct{
+		{Visual, true}, {Sound, true}}},
+	{[5]uint16{0, 24, 8, 0, 0}, Fruit, "Daydream Pomme", 75, []TraitStruct{
+		{Visual, true}, {Sound, true}}},
 	{[5]uint16{0, 16, 0, 0, 48}, Fruit, "Geode Citrus", 94, []TraitStruct{}},
+	{[5]uint16{0, 0, 0, 76, 0}, Fruit, "Slaughtermelon", 105, []TraitStruct{
+		{Visual, false}}},
+	{[5]uint16{0, 22, 0, 0, 66}, Fruit, "Dragonegg Citrus", 124, []TraitStruct{}},
 	{[5]uint16{48, 0, 48, 24, 24}, Fruit, "Charredonnay", 260, []TraitStruct{
 		{Taste, false}}},
 
@@ -110,12 +127,19 @@ var Ingredients = []Ingredient{
 		{Visual, false}, {Sound, true}}},
 	{[5]uint16{0, 0, 32, 0, 0}, Bug, "Rotfly Matriarch", 65, []TraitStruct{
 		{Taste, true}}},
+	{[5]uint16{0, 0, 44, 0, 0}, Bug, "Rotfly Mutant", 74, []TraitStruct{
+		{Taste, true}}},
 	{[5]uint16{0, 0, 0, 0, 48}, Bug, "Sepulcher Widow", 82, []TraitStruct{
 		{Visual, false}, {Sound, true}}},
 	{[5]uint16{0, 24, 24, 24, 0}, Bug, "Jeweled Scarab", 105, []TraitStruct{}},
+	{[5]uint16{0, 0, 0, 0, 66}, Bug, "Abominable Tarantula", 105, []TraitStruct{
+		{Visual, false}, {Sound, true}}},
+	{[5]uint16{0, 33, 33, 33, 0}, Bug, "Magma Beetle", 124, []TraitStruct{}},
 	{[5]uint16{96, 48, 0, 0, 0}, Bug, "Pegasus Mite", 134, []TraitStruct{
 		{Sensation, false}, {Sound, false}}},
 	{[5]uint16{24, 24, 32, 32, 0}, Bug, "Avalanche Cricket", 140, []TraitStruct{
+		{Taste, true}, {Sensation, false}}},
+	{[5]uint16{33, 33, 44, 44, 0}, Bug, "Frost Hopper", 196, []TraitStruct{
 		{Taste, true}, {Sensation, false}}},
 
 	// Fish
@@ -153,6 +177,7 @@ var Ingredients = []Ingredient{
 	{[5]uint16{16, 0, 0, 32, 0}, Flesh, "Harpy's Heart of Stone", 76, []TraitStruct{
 		{Sensation, true}}},
 	{[5]uint16{0, 0, 0, 48, 16}, Flesh, "Lamia's Shed Scales", 110, []TraitStruct{}},
+	{[5]uint16{0, 0, 0, 66, 22}, Flesh, "Body Snatcher's Sloughed Skin", 132, []TraitStruct{}},
 
 	// Bone
 	{[5]uint16{0, 0, 8, 0, 0}, Bone, "Unicorn Horn", 6, []TraitStruct{
@@ -182,6 +207,8 @@ var Ingredients = []Ingredient{
 	{[5]uint16{30, 20, 0, 0, 10}, Mineral, "Abyssalite", 79, []TraitStruct{}},
 	{[5]uint16{48, 32, 0, 16, 0}, Mineral, "Supernalite", 134, []TraitStruct{
 		{Taste, false}, {Visual, true}}},
+	{[5]uint16{55, 55, 0, 22, 0}, Mineral, "Hoarite", 167, []TraitStruct{
+		{Taste, false}, {Visual, true}}},
 
 	// Essence
 	{[5]uint16{4, 0, 4, 0, 0}, Essence, "Kappa Pheromones", 13, []TraitStruct{}},
@@ -189,11 +216,15 @@ var Ingredients = []Ingredient{
 	{[5]uint16{20, 0, 20, 0, 0}, Essence, "Nessie Pheromones", 50, []TraitStruct{}},
 	{[5]uint16{0, 10, 12, 18, 0}, Essence, "Raven's Shadow", 52, []TraitStruct{}},
 	{[5]uint16{0, 0, 30, 10, 0}, Essence, "Raiju Droppings", 55, []TraitStruct{}},
+	{[5]uint16{0, 33, 33, 11, 0}, Essence, "Dragon Tear", 999999, []TraitStruct{
+		{Sensation, true}}},
 	{[5]uint16{32, 0, 32, 0, 0}, Essence, "Ogre's Shadow", 74, []TraitStruct{}},
 	{[5]uint16{0, 0, 30, 20, 10}, Essence, "Dropspider's Shadow", 90, []TraitStruct{}},
 	{[5]uint16{0, 0, 64, 32, 0}, Essence, "Chimera Waste", 118, []TraitStruct{
 		{Aroma, false}}},
 	{[5]uint16{0, 48, 32, 16, 0}, Essence, "Bioplasm", 125, []TraitStruct{
+		{Visual, false}, {Sound, true}}},
+	{[5]uint16{0, 55, 55, 22, 0}, Essence, "Xenoplasm", 166, []TraitStruct{
 		{Visual, false}, {Sound, true}}},
 
 	// Gem
@@ -208,6 +239,8 @@ var Ingredients = []Ingredient{
 		{Visual, true}}},
 	{[5]uint16{30, 10, 20, 0, 0}, Gem, "Thunder Quartz", 72, []TraitStruct{}},
 	{[5]uint16{0, 32, 32, 0, 0}, Gem, "Griffin's-Whetstone Diamond", 86, []TraitStruct{}},
+	{[5]uint16{0, 0, 0, 44, 0}, Gem, "Dragonfire Pearl", 9999999, []TraitStruct{
+		{Visual, true}}},
 	{[5]uint16{64, 48, 0, 32, 0}, Gem, "Poison Quartz", 185, []TraitStruct{
 		{Sound, false}}},
 
@@ -218,9 +251,13 @@ var Ingredients = []Ingredient{
 	{[5]uint16{0, 0, 0, 30, 0}, Ore, "Fulgurite Ore", 40, []TraitStruct{}},
 	{[5]uint16{0, 0, 16, 0, 0}, Ore, "Celestial Ore", 45, []TraitStruct{
 		{Aroma, true}, {Visual, true}}},
+	{[5]uint16{0, 0, 22, 0, 0}, Ore, "Nether Ore", 51, []TraitStruct{
+		{Aroma, true}, {Visual, true}}},
 	{[5]uint16{0, 32, 64, 0, 0}, Ore, "Weeping Metal Ore", 66, []TraitStruct{
 		{Sensation, false}}},
 	{[5]uint16{30, 10, 0, 0, 20}, Ore, "Malachite Ore", 93, []TraitStruct{}},
+	{[5]uint16{66, 66, 0, 0, 33}, Ore, "Amethyst Ore", 206, []TraitStruct{
+		{Sound, false}}},
 
 	// Pure Mana
 	{[5]uint16{15, 15, 15, 15, 15}, PureMana, "Mote of Mana", 130, []TraitStruct{}},
--- src/potions.go
+++ src/potions.go
@@ -16,21 +16,25 @@ var Potions = []Potion{
 	{[5]uint16{0, 1, 1, 0, 0}, PPT, "Mana"},
 	{[5]uint16{1, 0, 0, 0, 1}, PPT, "Stamina"},
 	{[5]uint16{0, 0, 1, 1, 0}, PPT, "Speed"},
+	{[5]uint16{0, 0, 0, 1, 1}, PPT, "Tolerance"},
 	// Tonics
 	{[5]uint16{1, 0, 1, 0, 0}, TPT, "Fire"},
 	{[5]uint16{1, 0, 0, 1, 0}, TPT, "Ice"},
 	{[5]uint16{0, 1, 0, 1, 0}, TPT, "Thunder"},
 	{[5]uint16{0, 1, 0, 0, 1}, TPT, "Shadow"},
+	{[5]uint16{0, 0, 1, 0, 1}, TPT, "Radiation"},
 	// Enhancers
 	{[5]uint16{3, 4, 3, 0, 0}, EPT, "Sight"},
 	{[5]uint16{0, 3, 4, 3, 0}, EPT, "Alertness"},
 	{[5]uint16{4, 3, 0, 0, 3}, EPT, "Insight"},
 	{[5]uint16{3, 0, 0, 3, 4}, EPT, "Dowsing"},
+	{[5]uint16{0, 0, 3, 4, 3}, EPT, "Seeking"},
 	// Cures
 	{[5]uint16{2, 0, 1, 1, 0}, CPT, "Poison"},
 	{[5]uint16{1, 1, 0, 2, 0}, CPT, "Drowsiness"},
 	{[5]uint16{1, 0, 2, 0, 1}, CPT, "Petrification"},
 	{[5]uint16{0, 2, 1, 0, 1}, CPT, "Silence"},
+	{[5]uint16{0, 1, 1, 0, 2}, CPT, "Curse"},
 }
 
 type PotionKind byte
